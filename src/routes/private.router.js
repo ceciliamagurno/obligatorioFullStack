80b983759749438acb6cdf790a2c1f414dd74762
cecilemagurno <cecilia.magurno.11@gmail.com>
2025-10-09 17:53:03 -0300

    ultimo

--- src/routes/private.router.js
+++ src/routes/private.router.js
@@ -9,12 +9,12 @@ const router = express.Router();
 
 router.get('/books', getBooksController);
 router.get('/books/:id', getBookByIdController);
-// validar payload, comprobar límite por plan, luego crear libro
+
 router.post('/books', payloadMiddleWare(bookSchema), planLimitMiddleware, postBookController);
 router.put('/books/:id', payloadMiddleWare(bookSchema), putBookController);
 router.delete('/books/:id', deleteBookController);
 
-// Ruta para cambiar plan: sólo permite cambiar a 'premium' 
+
 router.post('/profile/plan', payloadMiddleWare(profileSchema), changePlanController);
 
 module.exports = router;
